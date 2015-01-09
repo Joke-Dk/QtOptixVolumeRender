95a6f99265cb9d66150a73173301104a9baddc76
joke-dk <dking@live.com>
2015-01-09 13:56:00 +0800

    [Fix bug] the self-define generate rule modified: copy debug/ptx into .exe

--- glwidget/main.cpp
+++ glwidget/main.cpp
@@ -45,7 +45,7 @@
 
 int main(int argc, char **argv)
 {
-	
+
 	//////////////////////////////////////////////////////////////////////////
 	//  Display debug information in console window.
 	AllocConsole();
