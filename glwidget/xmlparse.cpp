4c325f421ee3faa2b38466d5789c927a405dae89
Joke-Dk <dking@live.com>
2015-02-07 17:37:46 +0800

    [ .xml] add the string2float3 parser

--- glwidget/xmlparse.cpp
+++ glwidget/xmlparse.cpp
@@ -2,6 +2,29 @@
 #include <iostream>
 using namespace tinyxml2;
 
+optix::float3 Chars2Float3( const char* str0)
+{
+	optix::float3 ret;
+	std::string str( str0);
+	std::string tmpStr;
+	std::size_t pos1;
+	// float - x
+	pos1 = str.find_first_of(",");
+	tmpStr = str.substr( 0, pos1);
+	ret.x = atof(const_cast<const char *>(tmpStr.c_str()));
+	str = str.substr(pos1+1);
+	// float - y
+	pos1 = str.find_first_of(",");
+	tmpStr = str.substr( 0, pos1);
+	ret.y = atof(const_cast<const char *>(tmpStr.c_str()));
+	str = str.substr(pos1+1);
+	// float - z
+	pos1 = str.find_first_of(",");
+	tmpStr = str.substr( 0, pos1);
+	ret.z = atof(const_cast<const char *>(tmpStr.c_str()));
+	return ret;
+}
+
 XmlParse::XmlParse(const char* xmlFilename):
 	m_xmlFilename( xmlFilename)
 {
@@ -43,12 +66,12 @@ void XmlParse::ParseSensor( optix::Context& optixCtx, tinyxml2::XMLElement *elem
 	{
 		optixCtx[element->Attribute( "name")] ->setFloat( element->FloatAttribute( "value"));
 	}
-	else if( elementName == std::string("integer"))
+	else if( elementName == std::string("int"))
 	{
-		optixCtx[element->Attribute( "name")] ->setFloat( element->IntAttribute( "value"));
+		optixCtx[element->Attribute( "name")] ->setInt( element->IntAttribute( "value"));
 	}
-	else if( elementName == std::string("lookAt"))
+	else if( elementName == std::string("float3"))
 	{
-
+		optixCtx[element->Attribute( "name")] ->setFloat( Chars2Float3(element->Attribute( "value")));
 	}
 }
